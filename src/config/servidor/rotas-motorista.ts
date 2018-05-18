5c9ba466e0ac26fbd7a9c26065e855db72322438
José Simão <jose.lucas.simao@gmail.com>
2018-05-19 00:16:51 +0100

    Fix bug rota motorista

--- src/config/servidor/rotas-motorista.ts
+++ src/config/servidor/rotas-motorista.ts
@@ -11,7 +11,7 @@ export class RotasMotorista {
         const controladorAuth = new UsuarioControlador();
         this.expressApp.post('/createUserAMotorista', this.localController.criarMotorista.bind(this.localController));
         this.expressApp.put('/updateMotoristas', controladorAuth.verificaLogin.bind(controladorAuth),this.localController.UpdateMotorista.bind(this.localController));
-        this.expressApp.delete('/deleteMotoristas', controladorAuth.verificaLogin.bind(controladorAuth),this.localController.DeleteMotorista.bind(this.localController));
+        this.expressApp.delete('/deleteMotoristas/:id', controladorAuth.verificaLogin.bind(controladorAuth),this.localController.DeleteMotorista.bind(this.localController));
         this.expressApp.get('/getMotorista/:id', controladorAuth.verificaLogin.bind(controladorAuth),this.localController.getMotoristaByID.bind(this.localController));
         this.expressApp.post('/getMotoristas', controladorAuth.verificaLogin.bind(controladorAuth), this.localController.getMotoristaNear.bind(this.localController));
         
